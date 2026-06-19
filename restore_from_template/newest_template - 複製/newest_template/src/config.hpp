b28513fea6c56900794437b53cb5d7e53ee046b4
yu-shen-0125 <yuyushen2007@gmail.com>
2026-06-20 04:03:22 +0800

    Remove local build files from repository

--- restore_from_template/newest_template - 複製/newest_template/src/config.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-/* === Global settings === */
-#define RANDOM_SEED 123
-
-/* === Drop pieces (for games without drops, defaults to 0) === */
-#ifndef NUM_HAND_TYPES
-#define NUM_HAND_TYPES 0
-#endif
